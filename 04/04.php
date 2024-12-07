c553b2b21cd3de7c0e3e4fe5e27d5b3a06b437ea
Laurens Hoek <lrhoek@gmail.com>
2024-12-07 20:37:13 +0100

    Day 4 Part 2

--- 04/04.php
+++ 04/04.php
@@ -23,3 +23,28 @@ $count += array_sum(array_map(fn ($line) => search($word, $line), array_map(null
 $count += array_sum(array_map(fn ($line) => search($word, $line), array_map(null, ...array_map(shift(...), array_reverse($grid), array_keys($grid)))));
 
 echo $count.PHP_EOL;
+
+$found = 0;
+foreach($grid as $x => $row) {
+
+    if ($x === 0 || $x === array_key_last($grid)) {
+        continue;
+    }
+
+    foreach (array_keys($row, "A") as $y) {
+
+        if ($y === 0 || $y === array_key_last($row)) {
+            continue;
+        }
+
+        $words = [
+            $grid[$x-1][$y-1] . $grid[$x+1][$y+1],
+            $grid[$x-1][$y+1] . $grid[$x+1][$y-1]
+        ];
+
+        if (array_all($words, fn ($word) => in_array($word, ["SM", "MS"]))) {
+            $found++;
+        }
+    }
+}
+echo $found.PHP_EOL;
