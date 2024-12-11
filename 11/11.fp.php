190fca12ea1e5bd7b60f179dd8473f81aa2600b0
Laurens Hoek <lrhoek@gmail.com>
2024-12-12 00:30:27 +0100

    Day 11 - FP

--- 11/11.fp.php
+++ 11/11.fp.php
@@ -1,29 +1,35 @@
 <?php
 
+declare(strict_types=1);
+
 require_once 'vendor/autoload.php';
 
 use Crell\fp;
 use Lrhoek\pf;
 
-function map($pebbles, $amount, $number) {
-    return match (true) {
-        $number === 0 => pf\addset($pebbles, 1, $amount),
-        strlen($number) % 2 === 0 =>
-            fp\pipe(
-                str_split($number, strlen($number) / 2),
-                fp\reduce($pebbles, fn ($new, $part) => pf\addset($new, (int) $part, $amount))
-            ),
-        default => pf\addset($pebbles, $number * 2024, $amount)
-    };
-}
-
-function solve(string $input, int $blinks) {
+function solve(string $input, int $blinks) : int {
     return fp\pipe(
         $input,
         fp\explode(" "),
         array_count_values(...),
         pf\iterate($blinks, fp\reduceWithKeys([], map(...))),
         array_sum(...)
+    );
+}
+
+function map(array $pebbles, int $amount, int $number) : array {
+    return match (true) {
+        $number === 0 => pf\addset($pebbles, 1, $amount),
+        strlen((string) $number) % 2 === 0 => split($pebbles, (string) $number, $amount),
+        default => pf\addset($pebbles, $number * 2024, $amount)
+    };
+}
+
+function split(array $pebbles, string $number, int $amount) : array {
+    return array_reduce(
+        str_split($number, strlen($number) / 2),
+        fn($new, $part) => pf\addset($new, (int) $part, $amount),
+        $pebbles
     );
 }
 
